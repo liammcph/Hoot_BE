c003a0953661caa99fc4cd6d103d5c578ab69b73
liammcph <liammcpheeters@gmail.com>
2026-04-30 22:10:27 -0400

    added the 3 routes

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -3,6 +3,48 @@ const verifyToken = require("../middleware/verify-token.js");
 const Hoot = require("../models/hoot.js");
 const router = express.Router();
 
+
+
+// POST /hoots
+
+router.post("/", verifyToken, async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const hoot = await Hoot.create(req.body);
+    hoot._doc.author = req.user;
+    res.status(201).json(hoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+
+// GET /hoots
+
+// controllers/hoots.js
+
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
+    res.status(200).json(hoots);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// GET /hoots/:hootId
+
+router.get("/:hootId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId).populate("author");
+    res.status(200).json(hoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
 // DELETE /hoots/:hootId
 router.delete("/:hootId", verifyToken, async (req, res) => {
   try {
